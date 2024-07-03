d05ae086b69e75d8f88e4e1e1d82bea626036876
TurcanuAlina <alina.turcanu93@gmail.com>
2024-07-03 20:58:27 +0300

    Collections.ex16

---
+++ src/ex16/Genre.java
@@ -0,0 +1,5 @@
+package ex16;
+
+public enum Genre {
+    NOVEL, HOWTO
+}
---
+++ src/ex16/Main.java
@@ -0,0 +1,21 @@
+package ex16;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Book b1 = new Book(2005,"sdfsdf",Genre.NOVEL,"sfgfdg");
+        Book b2 = new Book(2007,"sdfsdf",Genre.HOWTO,"sfgfdg");
+        System.out.println(b1.compareTo(b2));
+    }
+}
+
+
+
+
